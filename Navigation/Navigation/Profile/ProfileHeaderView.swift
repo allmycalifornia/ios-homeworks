b41298a000cd3cfb593ac1af0d3198a88fe2c61a
Boris Kravchenko <allmycalifornia@gmail.com>
2023-02-07 11:19:34 +0300

    Выполнены пункты 1-2 ДЗ

---
+++ Navigation/Navigation/Profile/ProfileHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileHeaderView.swift
+//  Navigation
+//
+//  Created by Борис Кравченко on 07.02.2023.
+//
+
+import UIKit
+
+class ProfileHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

09627704548bc76a4d3873f8ce274007972a12a7
Boris Kravchenko <allmycalifornia@gmail.com>
2023-02-09 11:49:19 +0300

    Косяк в 6 пункте ДЗ

--- Navigation/Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Navigation/Profile/ProfileHeaderView.swift
@@ -9,15 +9,7 @@ import UIKit
 
 class ProfileHeaderView: UIView {
     
+
     
-    private var avatarView = AvatarView(frame: CGRect(x: 16, y: 106, width: 100, height: 100))
-    
-        
-        func setupProfileHeaderView() {
-            addSubview(avatarView)
-            
-            avatarView.image = UIImage(named: "avatar.jpg")
-            avatarView.customize()
-        }
-    }
+}
 

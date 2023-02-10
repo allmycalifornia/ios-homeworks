21e6045e509caccd82158c27007abb838c83712c
Boris Kravchenko <allmycalifornia@gmail.com>
2023-02-10 11:03:23 +0300

    Выполнен пункт 6 ДЗ
    Все элементы отображаются согласно макету, без косяков

--- Navigation/Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Navigation/Profile/ProfileHeaderView.swift
@@ -9,99 +9,74 @@ import UIKit
 
 class ProfileHeaderView: UIView {
     
-    
-    class UserImageView: UIImageView {
         private let userImageView: UIImageView = {
-            let imageView = UIImageView()
-            imageView.translatesAutoresizingMaskIntoConstraints = false
+            let imageView = UIImageView(frame: CGRect(x: 16, y: 106, width: 100, height: 100))
             imageView.backgroundColor = .red
             imageView.image = UIImage(named: "avatar")
             imageView.contentMode = .scaleAspectFill
             imageView.backgroundColor = .black
             imageView.clipsToBounds = true
+            imageView.layer.cornerRadius = 50.0
+            imageView.layer.borderWidth = 3.0
+            imageView.layer.borderColor = UIColor.white.cgColor
+            imageView.layer.masksToBounds = true
         return imageView
         }()
-        
-        func customize() {
-            layer.cornerRadius = 50.0
-            layer.borderWidth = 3.0
-            layer.borderColor = UIColor.white.cgColor
-            layer.masksToBounds = true
-        }
-    }
     
     
-    class UserNameLabel: UILabel {
         private let userNameLabel: UILabel = {
-            let label = UILabel()
+            let label = UILabel(frame: CGRect(x: 132, y: 117, width: 200, height: 20))
             label.text = "Rumi Hiiragi"
             label.textColor = .black
             label.font = UIFont.systemFont(ofSize: 18, weight: .bold)
         return label
         }()
-    }
     
     
-    class UserStatusLabel: UILabel {
-        private let userStatusLabel: UILabel = {
-            let label = UILabel()
+        private lazy var userStatusLabel: UILabel = {
+            let label = UILabel(frame: CGRect(x: 132, y: 186, width: 200, height: 20))
             label.text = "Waiting for something..."
             label.textColor = .gray
             label.font = UIFont.systemFont(ofSize: 14, weight: .regular)
             return label
         }()
-    }
+        
     
-    
-    class ShowStatusButton: UIButton {
         private let showStatusButton: UIButton = {
-            lazy var button = UIButton(type: .system)
+            var button = UIButton(type: .system)
+            button = UIButton(frame: CGRect(x: 16, y: 220, width: 358, height: 50))
             button.setTitle("Show status", for: .normal)
             button.setTitleColor(.white, for: .normal)
             button.backgroundColor = .systemBlue
             button.layer.shadowOffset = CGSize(width: 4, height: 4)
-            button.layer.cornerRadius = 4
+            button.layer.cornerRadius = 16   // в задании написано 4, но на макете углы гораздо более скруглённые
             button.layer.shadowColor = UIColor.black.cgColor
             button.layer.shadowOpacity = 0.7
-            button.addTarget(ProfileHeaderView.ShowStatusButton.self, action: #selector(statusButtonAction), for: .touchUpInside)
+            button.addTarget(ProfileHeaderView.self, action: #selector(statusButtonAction), for: .touchUpInside)
             return button
         }()
-    }
-    
-    
-    private let userImageView = UserImageView(frame: CGRect(x: 16, y: 106, width: 100, height: 100))
-    private let userNameLabel = UserNameLabel(frame: CGRect(x: 132, y: 117, width: 150, height: 20))
-    private let userStatusLabel = UserStatusLabel(frame: CGRect(x: 132, y: 195, width: 150, height: 20))
-    private let showStatusButton = ShowStatusButton(frame: CGRect(x: 50, y: 50, width: 200, height: 50))
     
     func setupProfileHeaderView() {
         addSubview(userImageView)
-        userImageView.customize()
         addSubview(userNameLabel)
         addSubview(userStatusLabel)
         addSubview(showStatusButton)
     }
     
-//    init() {
-//        super.init (frame: .zero)
-//        setupProfileHeaderView()
-//    }
-//
-//    required init?(coder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-    
     override init(frame: CGRect) {
-        super.init(frame: frame)
-        setupProfileHeaderView()
-    }
-
+           super.init (frame: .zero)
+           setupProfileHeaderView()
+       }
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
     @objc func statusButtonAction() {
-        print(userStatusLabel.text!)
+        print("Waiting for something...")
     }
+    
+    
+    
 }
 

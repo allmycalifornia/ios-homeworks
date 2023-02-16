83e5e814903c5cbf3cfa25de708997c7aad179fc
Boris Kravchenko <allmycalifornia@gmail.com>
2023-02-16 15:58:50 +0300

    Выполнены пункты 1-2 ДЗ
    1. Создайте модель публикации Post, она должна содержать следующие поля: author: String - никнейм автора публикации description: String - текст публикации image: String - имя картинки из каталога Assets.xcassets likes: Int - количество лайков views: Int - количество просмотров
    2. Создайте массив из минимум четырех публикаций. Все данные, в том числе изображения для публикаций, используйте на свой вкус.

--- Navigation/Navigation/Post.swift
+++ Navigation/Navigation/Post.swift
@@ -7,7 +7,32 @@
 
 import UIKit
 
-struct Post {
-    var title: String
+struct Posts {
+    let author: String
+    let image: UIImage
+    var description: String
+    var likes: Int
+    var views: Int
+    
+    static func makePost() -> [[Posts]] {
+        var post = [[Posts]]()
+        var section = [Posts]()
+        section.append(Posts(author: "Лариса Мосенко", image: UIImage(named: "TomHolland")!, description: "Том Холланд вернётся к роли Человека-паука в MCU", likes: Int.random(in: 300...500), views: Int.random(in: 700...1999)))
+        section.append(Posts(author: "Юрий Лущинский", image: UIImage(named: "Dragon")!, description: "«Как приручить дракона» станет полнометражным фильмом", likes: Int.random(in: 300...500), views: Int.random(in: 700...1999)))
+        section.append(Posts(author: "Лариса Мосенко", image: UIImage(named: "Andor")!, description: "Диего Луна и Стеллан Скарсгард засветились на съёмках второго сезона «Андора»", likes: Int.random(in: 300...500), views: Int.random(in: 700...1999)))
+        section.append(Posts(author: "Дина Касимова", image: UIImage(named: "PaulWalker")!, description: "Режиссёр «Форсажа 10» рассказал о возвращении Пола Уокера", likes: Int.random(in: 300...500), views: Int.random(in: 700...1999)))
+        
+//        for _ in 0...5 {
+//            post.append(section)
+//        }
+        return post
+    }
 }
 
+
+/* Ссылки на новости:
+ https://kg-portal.ru/comments/112082-tom-holland-vernetsja-k-roli-chelovekapauka-v-mcu/
+ https://kg-portal.ru/comments/112090-zarazitelnye-disneevskie-primery-kak-priruchit-drakona-stanet-polnometrazhnym-filmom/
+ https://kg-portal.ru/comments/112079-diego-luna-i-stellan-skarsgard-zasvetilis-na-semkah-vtorogo-sezona-andora-foto/
+ https://kg-portal.ru/comments/112067-rezhisser-forsazha-10-rasskazal-o-vozvraschenii-geroja-pola-uokera/
+ */

710b16df3ae237fc1e08f4e0e350abdb03a55876
Boris Kravchenko <allmycalifornia@gmail.com>
2023-03-19 14:42:23 +0300

    added gesture and animation for userImage on ProfileViewController

--- Navigation/Navigation/Profile/ProfileViewController.swift
+++ Navigation/Navigation/Profile/ProfileViewController.swift
@@ -34,7 +34,6 @@ final class ProfileViewController: UIViewController {
     
     private func layout() {
             view.addSubview(tableView)
-            tableView.tableHeaderView = ProfileTableHeaderView(frame: CGRect(x: 0, y: 0, width: tableView.frame.width, height: 220))
             tableView.backgroundColor = .systemGray4
         
         NSLayoutConstraint.activate([
